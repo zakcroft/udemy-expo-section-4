d4d7483880778f655040906aa805aa648f40787d
Zak Croft <zak.croft@mokriya.com>
2020-10-26 16:13:07 +0000

    section 4

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,19 +1,58 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+
+import Card from "./Card";
+import Input from "./Input";
+import COLORS from "../constants/colors";
 
 const StartGameScreen = (props) => {
+  console.log("props", props);
+
+  const [enteredValue, setEnteredValue] = useState("");
+  const numberedInputHandler = (inputText) => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a new Game</Text>
-      <View style={styles.inputContainer}>
-        <Text>Select a number</Text>
-        <TextInput />
-        <View style={styles.buttonContainer}>
-          <Button title="Reset" onPress={() => {}} />
-          <Button title="Confirm" onPress={() => {}} />
-        </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a new Game</Text>
+        <Card style={{ width: "80%", alignItems: "center" }}>
+          <Text>Select a number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="number-pad"
+            maxLength={2}
+            onChangeText={numberedInputHandler}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button title="Reset" onPress={() => {}} color={COLORS.accent} />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title="Confirm"
+                onPress={() => {}}
+                color={COLORS.primary}
+              />
+            </View>
+          </View>
+        </Card>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -23,9 +62,22 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems: "center",
   },
-  title: {},
-  inputContainer: {},
-  buttonContainer: {},
+  title: {
+    fontSize: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  button: {
+    width: 100,
+  },
+  input: {
+    width: 70,
+    textAlign: "center",
+  },
 });
 
 export default StartGameScreen;
